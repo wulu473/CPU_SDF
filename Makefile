a39af0ce8539006d176eb458c60c9b161e62a7a7
Lukas Wutschitz <lw473@cam.ac.uk>
2018-07-05 18:54:01 +0100

    Tests: Move to boost test suite and add more tests

--- Makefile
+++ Makefile
@@ -1,6 +1,10 @@
 
 test: test.C SignedDistance.C 2DSDF.C
-	g++-6 -O0 -g -Wextra  -Wall -pedantic -std=c++11  $^ -o $@
+	@echo "Compiling..."
+	g++-6 -O0 -g -Wextra  -Wall -pedantic -std=c++11 \
+	       	-I/lsc/opt/modules/gcc-6.4.0/boost-1.63.0/include/ \
+		$^ -o $@ \
+		-lboost_unit_test_framework -L/lsc/opt/modules/gcc-6.4.0/boost-1.61.0/lib  
 
 .PHONY: clean
 clean:
--- test.C
+++ test.C
@@ -1,23 +1,129 @@
+#define BOOST_TEST_MODULE Main
+#define BOOST_TEST_DYN_LINK
+
 #include <cmath>
 #include <cassert>
 #include <limits>
+#include <boost/test/unit_test.hpp>
 
 void getSDF(double* sdf, double xMin, double yMin,
     double dx, double dy, int left, int right, int bottom, int top,
     const double* vertices, int numVertices, double maxDist);
 
-int main(int, char **)
+BOOST_AUTO_TEST_SUITE(SignedDistanceTest)
+
+BOOST_AUTO_TEST_CASE(PolygonTest)
 {
-  double triangle[6] = {-1,0,
-                         0,1,
-                         1,0};
+  double poly[10] = {0,0,
+                     0,2,
+                     1,2,
+                     2,1,
+                     1,0};
 
-  // Test 1
-  // Check if a point on an edge of a triangle has distanc e0
-  double sdf = std::numeric_limits<double>::signaling_NaN();
-  getSDF(&sdf,-0.5,-0.5,1.0,1.0,0,0,1,1,triangle,3,100);
-  assert(fabs(sdf) < 1e-10);
+  auto distance = [poly](const double x, const double y)
+  {
+    double sdf = std::numeric_limits<double>::signaling_NaN();
+    getSDF(&sdf,x-0.5,y-0.5,1.0,1.0,0,0,1,1,poly,5,100);
+    return sdf;
+  };
 
-  return 0;
+  // Point inside polygon OK
+  BOOST_CHECK_GT(distance(0.5,1), 0);
+
+  BOOST_CHECK_CLOSE(distance(0.5,1), 0.5, 1e-5);
+
+  // Point inside polygon OK
+  BOOST_CHECK_GT(distance(1.5,1), 0);
+
+  // Shortest distance goes along line perp. to sloping side
+  BOOST_CHECK_CLOSE(distance(1.5,1), 0.5 * sqrt(2*0.5*0.5), 1e-5);
+  
+  // Point outside polygon OK
+  BOOST_CHECK_LT(distance(2,0), 0 );
+
+  // Shortest distance goes along normal to sloping side
+  BOOST_CHECK_CLOSE(distance(2,0), - 0.5 * sqrt(2*1*1), 1e-5 );
+  
+  // Point outside polygon OK
+  BOOST_CHECK_LT(distance(0.5, 2.1), 0 );
+
+  BOOST_CHECK_CLOSE(distance(0.5, 2.1), -0.1, 1e-5 );
+  
+  // Point on polygon OK
+  BOOST_CHECK_SMALL(distance(1.4, 1.6), (double)0.0001);
+
+  // Point on polygon OK
+  BOOST_CHECK_SMALL(distance(1.4, 0.4), (double)0.0001);
 }
+
+
+BOOST_AUTO_TEST_CASE(PolygonConcaveTest)
+{
+  double poly[10] = {0,0,
+                     0,2,
+                     1,1,
+                     2,2,
+                     2,0};
+
+  auto distance = [poly](const double x, const double y)
+  {
+    double sdf = std::numeric_limits<double>::signaling_NaN();
+    getSDF(&sdf,x-0.5,y-0.5,1.0,1.0,0,0,1,1,poly,5,100);
+    return sdf;
+  };
+
+  // Point (0.5,0.5) inside polygon OK
+  BOOST_CHECK_GT(distance(0.5,0.5), 0);
+
+  BOOST_CHECK_CLOSE(distance(0.5,0.5), 0.5,1e-5);
+
+  // Point (1.5, 0.5) inside polygon
+  BOOST_CHECK_GT(distance(1.5,0.5), 0);
+
+  // Closest distance goes to vertical side
+  BOOST_CHECK_CLOSE(distance(1.5,0.5), 0.5, 1e-5);
+  
+  // Point (0.4,1.4) inside polygon
+  BOOST_CHECK_GT(distance(0.4,1.4), 0);
+
+  // Shortest distance goes along perp. to sloping side
+  BOOST_CHECK_CLOSE(distance(0.4,1.4), sqrt(2*0.1*0.1), 1e-5);
+  
+  // Point (1.6,1.4) inside polygon
+  BOOST_CHECK_GT(distance(1.6,1.4), 0);
+
+  // Shortest distance goes along perp. to sloping side
+  BOOST_CHECK_CLOSE(distance(1.6,1.4), sqrt(2*0.1*0.1), 1e-5);
+  
+  // Point (1,1.5) outside polygon
+  BOOST_CHECK_LT(distance(1,1.5), 0);
+
+  // Shortest dist. goes along perp. to sloping side
+  BOOST_CHECK_CLOSE(distance(1,1.5), -0.5*sqrt(2*0.5*0.5), 1e-5);
+
+  // Point (3,1) outside polygon
+  BOOST_CHECK_LT(distance(3,1), 0);
+
+  BOOST_CHECK_CLOSE(distance(3,1), -1, 1e-5);
+
+  // Check (0.4,1.6) on polygon
+  BOOST_CHECK_SMALL(distance(0.4,1.6), (double)0.0001);
+
+  // Check (1.6,1.6) on polygon
+  BOOST_CHECK_SMALL(distance(1.6,1.6), (double)0.0001);
+
+  // Check that shortest distance from (1,3) to polygon goes to one of the top vertices
+  BOOST_CHECK_CLOSE(distance(1,3), -sqrt(2), 1e-5);
+
+  // Check that shortest dist. from (-0.3,-0.1) goes to bottom left vertex
+  BOOST_CHECK_CLOSE(distance(-0.3,-0.1), -sqrt(0.3*0.3 + 0.1*0.1), 1e-5);
+
+  // Check that nearest point to (1,0.9) is concave point (not lower line, for example)
+  BOOST_CHECK_CLOSE(distance(1,0.9), 0.1, 1e-5);
+  
+  // Check that dist. to pt directly in line with an edge-line is OK.
+  BOOST_CHECK_CLOSE(distance(2,-0.2), -0.2, 1e-5);
+}
+
+BOOST_AUTO_TEST_SUITE_END()
 
